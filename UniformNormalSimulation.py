28cdf516de5a4a8e5731363ddf6a735aa15b4b49
AlifsyahRS <alifsyah.sutarjadi@binus.ac.id>
2021-06-16 16:07:47 +0700

    Added normal continuous distribution and a legend for the graphs

--- UniformNormalSimulation.py
+++ UniformNormalSimulation.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
+from scipy.stats import norm
+from scipy.stats import uniform
 
 
 
@@ -15,26 +16,26 @@ buttons_frame = tk.Frame(root)
 tabControl = ttk.Notebook(buttons_frame)
 
 #Formula for distributions
-uniform = lambda a,b: 1 / (b-a) # Formula for uniform distribution
-normal = lambda c,mu,std: (1 / (std * np.sqrt(2 * np.pi))) * np.exp( -0.5 * (c - mu / std) ** 2) #Formula for nomral distribution
+uniformf = lambda a,b: 1 / (b-a) # Formula for uniform distribution
+normalf = lambda c,mu,std: (1 / (std * np.sqrt(2 * np.pi))) * np.exp( -0.5 * (c - mu / std) ** 2) #Formula for nomral distribution
 
 
 #Functions
 
 def normalPdf(c,mean,std):
-    return normal(c,mean,std)
+    return normalf(c,mean,std)
 
 def uniformPdf(c,a,b):
-    if c>=a and c<=b:
-        return uniform(a,b)
+    if c>=a or c<=b:
+        return uniformf(a,b)
     else:
         return 0
 
-def normalCdf(c,d,mean,std):
-    pass
+def normalCdf(c,mean,std):
+    return norm.cdf(c, mean, std)
 
-def uniformCdf(c,d,a,b):
-    pass
+def uniformCdf(c,a,b):
+    return uniform.cdf(c, a, b)
 
 
 
@@ -64,25 +65,25 @@ def calcUpd():
     a = int(aVar.get())
     b = int(bVar.get())
     calcResult = uniformPdf(c,a,b)
-    tk.Label(tabUpd,text=f"Calculated  results: {calcResult}").grid(row=7,column=1)
+    tk.Label(tabUpd,text=f"Calculated  results: {calcResult}").grid(row=8,column=1)
     for widgets in graphFrame.winfo_children():
         widgets.destroy()
     #Creating the figure for the graphs
     fig = plt.figure(111)
     plt.clf()
     ax = fig.add_subplot(111)
-    ax = fig.add_subplot(111)
     if showGraphUpd.get() == 1:
-        x = np.linspace(a,b,10000)
+        x = np.linspace(a,b,1000)
         y = np.zeros(x.shape[0])
-        y = y+uniform(a,b)
-        ax.set_title("Normal Probabilty Distribution")
+        y = y+uniformf(a,b)
+        ax.set_title("Uniform Probabilty Distribution")
         ax.plot(x,y)
-        ax.plot(c,calcResult,'o',label="P(X=C)")
+        ax.plot(c,calcResult,'o',label="C")
         graph = FigureCanvasTkAgg(fig,graphFrame)
+        ax.legend()
         graph.get_tk_widget().pack()
     
-updCalcButton = tk.Button(tabUpd,text="Calculate",command=calcUpd).grid(row=6,column=1)
+updCalcButton = tk.Button(tabUpd,text="Calculate",command=calcUpd).grid(row=7,column=1)
 
 #Uniform Continuous Distribution Tab
 showGraphUcd = tk.IntVar()
@@ -93,10 +94,10 @@ tk.Label(tabUcd,text="Lower Boundary (a)").grid(row=2)
 tk.Label(tabUcd,text="Upper Boundary (b)").grid(row=3)
 tk.Label(tabUcd,text="C").grid(row=4)
 tk.Label(tabUcd,text="D").grid(row=5)
-entryAUpd = tk.Entry(tabUcd).grid(row=2,column=1)
-entryBUpd = tk.Entry(tabUcd).grid(row=3,column=1)
-entryCUpd = tk.Entry(tabUcd).grid(row=4,column=1)
-entryDUpd = tk.Entry(tabUcd).grid(row=5,column=1)
+entryAUcd = tk.Entry(tabUcd,textvariable=aVar).grid(row=2,column=1)
+entryBUcd = tk.Entry(tabUcd,textvariable=bVar).grid(row=3,column=1)
+entryCUcd = tk.Entry(tabUcd,textvariable=cVar).grid(row=4,column=1)
+entryDUcd = tk.Entry(tabUcd,textvariable=dVar).grid(row=5,column=1)
 checkButtonUcd = tk.Checkbutton(tabUcd,text="Show Graph",variable=showGraphUcd,onvalue=1,offvalue=0).grid(row=6,column=1)
 
 def calcUcd():
@@ -104,6 +105,27 @@ def calcUcd():
     d = int(dVar.get())
     a = int(aVar.get())
     b = int(bVar.get())
+    calcResult = uniformCdf(d,a,b) - uniformCdf(c,a,b)
+    tk.Label(tabUcd,text=f"Calculated  results: {calcResult}").grid(row=8,column=1)
+    for widgets in graphFrame.winfo_children():
+        widgets.destroy()
+    #Creating the figure for the graphs
+    fig = plt.figure(111)
+    plt.clf()
+    ax = fig.add_subplot(111)
+    if showGraphUcd.get() == 1:
+        x = np.linspace(a,b,1000)
+        xfill = [c,d]
+        y = np.zeros(x.shape[0])
+        y = y+uniformf(a,b)
+        ax.set_title("Uniform Continuous Distribution")
+        ax.plot(x,y)
+        ax.plot(c,uniformf(a,b),'o--',label="C")
+        ax.plot(d,uniformf(a,b),'o--',label="D")
+        ax.fill_between(xfill, uniformf(a,b), alpha = 0.5)
+        ax.legend()
+        graph = FigureCanvasTkAgg(fig,graphFrame)
+        graph.get_tk_widget().pack()
 
 ucdCalcButton = tk.Button(tabUcd,text="Calculate",command=calcUcd).grid(row=7,column=1)
 
@@ -134,10 +156,10 @@ def calcNpd():
     plt.clf()
     ax = fig.add_subplot(111)
     if showGraphNpd.get() == 1:
-        x = np.linspace(c-20,c+20,10000)
+        x = np.linspace(c-10,c+10,1000)
         ax.set_title("Normal Probability Distribution")
-        ax.plot(x,normal(x,mean,std))
-        ax.plot(c,normal(c,mean,std),'o',label="P(X=C)")
+        ax.plot(x,normalf(x,mean,std))
+        ax.plot(c,normalf(c,mean,std),'o',label="P(X=C)")
         ax.legend()
         graph = FigureCanvasTkAgg(fig,graphFrame)
         graph.get_tk_widget().pack()
@@ -161,14 +183,35 @@ entryDNcd = tk.Entry(tabNcd,textvariable=dVar).grid(row=3,column=1)
 entryMeanNcd = tk.Entry(tabNcd,textvariable=aVar).grid(row=4,column=1)
 entryStdNcd = tk.Entry(tabNcd,textvariable=bVar).grid(row=5,column=1)
 checkButtonNcd = tk.Checkbutton(tabNcd,text="Show Graph",variable=showGraphNcd,onvalue=1,offvalue=0).grid(row=6,column=1)
-resultsLabel = tk.Label(tabNcd,text="").grid(row=8,column=1)
+
 
 def calcNcd():
     c = int(cVar.get())
     d = int(dVar.get())
     mean = float(aVar.get())
     std = float(bVar.get())
+    calcResult = normalCdf(d,mean,std) - normalCdf(c,mean,std)
+    tk.Label(tabNcd,text=f"Calculated results: {calcResult}").grid(row=8,column=1)
+    for widgets in graphFrame.winfo_children():
+        widgets.destroy()
+    #Creating the figure for the graphs
+    fig = plt.figure(111)
+    plt.clf()
+    ax = fig.add_subplot(111)
+    if showGraphNcd.get() == 1:
+        x = np.linspace(c-10,d+10,10000)
+        xfill = np.linspace(c,d,1000)
+        ax.set_title("Normal Continuous Distribution")
+        ax.plot(x,normalf(x,mean,std))
+        ax.plot(c,normalf(c,mean,std),'o--',label="C")
+        ax.plot(d,normalf(d,mean,std),'o--',label="D")
+        ax.fill_between(xfill,normalf(xfill,mean,std),alpha=0.5)
+        ax.legend()
+        graph = FigureCanvasTkAgg(fig,graphFrame)
+        graph.get_tk_widget().pack()
 
+        
+    
 
 ncdCalcButton= tk.Button(tabNcd,text="Calculate",command=calcNcd).grid(row=7,column=1)
 
